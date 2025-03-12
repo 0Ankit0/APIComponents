a266c9fc75431265e4ce3cae3857785d4458e6e7
0ankit0 <ankitpaudel.1373@gmail.com>
2025-03-12 07:26:56 +0545

    Add LoggingApi project with default weather forecast API
    This commit introduces a new ASP.NET Core Web API project named `LoggingApi`. It includes a `WeatherForecastController` that handles weather forecast requests using dependency injection for logging. The project targets .NET 9.0 and incorporates necessary packages for OpenAPI support. Additionally, the solution file is updated to include the new project, and various configuration files are modified to set up logging and application settings.

--- Logging/LoggingService/LoggingService.AppHost/Program.cs
+++ Logging/LoggingService/LoggingService.AppHost/Program.cs
@@ -1,3 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var apiProject = builder.AddProject<Projects.LoggingApi>("api")
+                    .WithExternalHttpEndpoints();
+                    
+
 builder.Build().Run();
